204f49735649f3d90570ffadf1065e90ad9ed453
jesus.carrillo179 <chuchitopimp@hotmail.com>
2020-05-07 10:39:24 -0500

    done! no stretch

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const userRouter = require('./users/userRouter')
+const postRouter = require('./posts/postRouter')
 
 const server = express();
 
-server.get('/', (req, res) => {
+server.use(express.json())
+
+server.use('/api/users', logger, userRouter)
+server.use('/api/posts', logger, postRouter)
+
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const timestamp = new Date().toTimeString()
+  console.log(`\n REQUEST: ${req.method} to ${req.url} timestamp: ${timestamp} \n`)
+  next()
+}
 
 module.exports = server;
